87d2983a315008bb76694f98773d3ae301bc6e28
Andrew Richter <andrewrichter1@hotmail.com>
2024-10-15 22:14:34 -0300

    added forms validation

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car, Brand
-from .forms import CarForm
+from .forms import CarModelForm
 
 def cars_view(request):
     brands = Brand.objects.all()
@@ -21,8 +21,14 @@ def cars_view(request):
     return render(request, "cars.html", {"brands": brands})
         
 def new_car_view(request):
-    new_car_form = CarForm()
-    context = {
-        'car_form' : new_car_form,
-    }
+    context = {}
+    if request.method == 'POST':
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+        context['car_form'] = new_car_form
+    if request.method == 'GET':
+        new_car_form = CarModelForm()
+        context['car_form'] = new_car_form
     return render(request, "new_car.html", context)
