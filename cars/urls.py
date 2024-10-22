bfa943a1ed05df718d0904e0695e3993aee18ebd
AndrewDRichter <andrewrichter1@hotmail.com>
2024-10-22 15:02:10 -0300

    CRUD finished

--- cars/urls.py
+++ cars/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import CarsListView, NewCarCreateView, CarDetailView
+from .views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView, CarDeleteView
 
 
 urlpatterns = [
     path("", CarsListView.as_view(), name='cars_list'),
     path("new", NewCarCreateView.as_view(), name='new_car'),
     path("<int:pk>", CarDetailView.as_view(), name='car_detail'),
+    path("<int:pk>/update", CarUpdateView.as_view(), name='car_update'),
+    path("<int:pk>/delete", CarDeleteView.as_view(), name='car_delete'),
 ]
