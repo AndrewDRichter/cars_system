b66defba04ee44355298ebcd00991a817398ea83
AndrewDRichter <andrewrichter1@hotmail.com>
2024-10-24 17:20:21 -0300

    m

--- cars/signals.py
+++ cars/signals.py
@@ -2,6 +2,10 @@ from django.db.models.signals import pre_save, pre_delete, post_save, post_delet
 from django.dispatch import receiver
 from .models import Car
 
-@receiver(signal=pre_save, sender=Car)
+@receiver(signal=post_save, sender=Car)
 def car_pre_save(sender, instance, **kwargs):
-    print("### PRE SAVE ###")
+    cars_count = Car.objects.all().count()
+
+@receiver(signal=post_delete, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    print("### POST DELETE ###")
